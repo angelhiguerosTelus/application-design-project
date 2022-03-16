1a7c3e1d1c4503042ec1e316ed3548ba342a491b
Angel Higueros <99747585+angelhiguerosTelus@users.noreply.github.com>
2022-03-15 18:57:56 -0600

    created components to show and update user info (#3)

--- src/pages/home/homeScreen.js
+++ src/pages/home/homeScreen.js
@@ -1,11 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 import { Menu } from "../ui/menu";
+import { UpdateUserInfo } from "./updateUserInfo";
+import { UserInfo } from "./userInfo";
 
 export const HomeScreen = () => {
+
+  const [viewUserUpdateInfo, setViewUserUpdateInfo] = useState(false)
+
   return (
     <>
-      <div className="container">
+      <div className="container container-app">
         <Menu />
+        <div className="box mt-5">
+          <UserInfo setView={setViewUserUpdateInfo}/>
+          <UpdateUserInfo view={viewUserUpdateInfo}/>
+        </div>
       </div>
     </>
   );
