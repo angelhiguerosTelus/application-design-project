db27cfebc8ab61205592229c2284159992c6aeab
Angel Higueros <99747585+angelhiguerosTelus@users.noreply.github.com>
2022-03-20 23:20:42 -0600

    feat: bd added (#12)

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,36 @@
 const express = require("express");
+const { getUser } = require("./src/bd/actionJson");
 const app = express();
 
 const { PORT } = require("./src/configs/server");
 
-app.use('/', )
+// Users
+app.post("/user", (req, res) => {
+  let { username, password } = req.body;
+
+  let response = getUser(username, password);
+  res.send({
+    error: "",
+    data: response,
+  });
+});
+
+app.post("/user/add", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.put("/user", (req, res) => {
+  res.send("Hello World!");
+});
+
+// Pokedex
+app.post("/pokemon", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.post("/pokemon/list", (req, res) => {
+  res.send("Hello World!");
+});
 
 app.listen(PORT, () => {
   console.log(`Application listening on port ${PORT}`);
